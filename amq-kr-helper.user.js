3be32c9855f7551ded1f45f519bc3d6eced7a82c
duf7317 <duf7317@naver.com>
2026-08-15 17:08:00 +0900

    Fix updater URL and release v1.8.1

--- amq-kr-helper.user.js
+++ amq-kr-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ KR Helper
 // @namespace    amq-kr-helper
-// @version      1.8.0
+// @version      1.8.1
 // @description  AMQ 원래 입력을 유지하면서 한글/영문/로마자 별칭 검색을 보조합니다.
 // @match        https://animemusicquiz.com/*
 // @match        https://www.animemusicquiz.com/*
@@ -13,14 +13,14 @@
 // @connect      docs.google.com
 // @connect      googleusercontent.com
 // @run-at       document-idle
-// @updateURL    https://raw.githubusercontent.com/duf7317/AMQ-Korean-Answer/main/amq-kr-helper.user.js
-// @downloadURL  https://raw.githubusercontent.com/duf7317/AMQ-Korean-Answer/main/amq-kr-helper.user.js
+// @updateURL    https://raw.githubusercontent.com/duf7317/AMQ-Korean-Answer/refs/heads/main/amq-kr-helper.user.js
+// @downloadURL  https://raw.githubusercontent.com/duf7317/AMQ-Korean-Answer/refs/heads/main/amq-kr-helper.user.js
 // ==/UserScript==
 
 (() => {
     'use strict';
 
-    const VERSION = '1.8.0';
+    const VERSION = '1.8.1';
     const DEFAULT_SHEET_URL = 'https://docs.google.com/spreadsheets/d/19-YDyMy__mPoP5Ozi7nPJ-A83XwsljODhhqmzuv1P2g/export?format=tsv&gid=0';
     const REFRESH_MS = 60_000;
     const MAX_RESULTS = 8;
